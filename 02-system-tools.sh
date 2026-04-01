04b707216e39dc5612c5d6acd2b1eba82492014c
David J. Sankel <dsankel@adobe.com>
2026-03-31 21:55:08 -0400

    Enable paccache.timer and zram-generator (8GB swap).

--- 02-system-tools.sh
+++ 02-system-tools.sh
@@ -10,3 +10,11 @@ AddPackage ripgrep # A search tool that combines the usability of ag with the ra
 AddPackage jq # Command-line JSON processor
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
 AddPackage --foreign paru # Feature packed AUR helper
+
+# Cache maintenance
+AddPackage pacman-contrib # Contributed scripts and tools for pacman systems
+CreateLink /etc/systemd/system/timers.target.wants/paccache.timer /usr/lib/systemd/system/paccache.timer
+
+# Swap management
+AddPackage zram-generator # Systemd-based generator for zram swap devices
+printf "[zram0]\nzram-size = 8192\n" > "$(CreateFile /etc/systemd/zram-generator.conf)"
